c81287d011983e92a1f6875ea290809d3189aad1
Gagandeep Singh <127825070+gagandeepsinghshinedezign@users.noreply.github.com>
2024-11-22 18:17:09 +0530

    Create testingInternet6.js

---
+++ testingInternet6.js
@@ -0,0 +1,51 @@
+function checkInternet() {
+    const existingMessage = document.querySelector('.connection-establish, .connection-lost');
+    if (existingMessage) {
+      existingMessage.remove();
+    }
+  let elem = document.createElement('div');
+
+  if (navigator.onLine) {
+    elem.className = 'connection-establish';
+    elem.innerHTML = '<p>You are back online!</p>';
+    document.body.appendChild(elem);
+
+    // Remove the element after 3 seconds
+    setTimeout(() => {
+      elem.remove();
+    }, 3000);
+  } else {
+    elem.className = 'connection-lost';
+    elem.innerHTML = `
+      <div class="lost-msg">
+        <button id="close-connection-btn">X</button>
+        <h2>Connection Error</h2>
+        <p>Your connection has been lost. Please check your network connection.</p>
+      </div>
+    `;
+    document.body.appendChild(elem);
+
+    // Add click event listener to the button
+    let closeButton = document.getElementById('close-connection-btn');
+    closeButton.onclick = () => {
+      elem.remove();
+    };
+  }
+}
+
+
+
+// Event listeners for online and offline events
+window.addEventListener('online', () => {
+  console.log("Back online");
+  checkInternet();
+});
+
+window.addEventListener('offline', () => {
+  console.log("You are offline");
+  checkInternet();
+});
+
+window.addEventListener('load', () => {
+    checkInternet(); // Check the current connection status
+  });
